af537f6aa328ab855cbf76d8969c5560ec216cfa
Soltan <104300557+MehdiyevSoltan2004@users.noreply.github.com>
2025-02-28 14:36:19 +0400

    Delete src/__init__.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.Face_Recognition import load_known_faces, recognize_faces_in_video
-
-if __name__ == "__main__":
-    known_faces_folder = "C:\\Users\\MSI\\Pictures\\Camera Roll\\"
-    known_face_encodings, known_face_names = load_known_faces(known_faces_folder)
-    recognize_faces_in_video(known_face_encodings, known_face_names)
